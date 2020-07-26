6760f31d1bd1f57e5b3f4de69c75ee3d28475119
seoorient <seoorient@gmail.com>
2020-07-26 22:58:36 +0300

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './auth/components/register/register.component';
 
 
 const routes: Routes = [];
