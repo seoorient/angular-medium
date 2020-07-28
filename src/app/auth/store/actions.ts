65be9b7b65d93809cbe550db548ef26f30b4a3ac
seoorient <seoorient@gmail.com>
2020-07-28 23:23:38 +0300

    add global feed

--- src/app/auth/store/actions.ts
+++ src/app/auth/store/actions.ts
@@ -1,6 +1,6 @@
 import { createAction, props } from '@ngrx/store';
-import { ActionTypes } from 'src/app/auth/store/actionTypes';
-import { RegisterRequestInterface } from 'src/app/auth/types/registerRequest.interface';
+import { ActionTypes } from '../store/actionTypes';
+import { RegisterRequestInterface } from '../types/registerRequest.interface';
 
 export const registerAction = createAction(
   ActionTypes.REGISTER,
