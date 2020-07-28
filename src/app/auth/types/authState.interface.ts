05883788a9fea6b25c943e28eb07ef8e835cccba
seoorient <seoorient@gmail.com>
2020-07-28 17:01:41 +0300

    add status users

--- src/app/auth/types/authState.interface.ts
+++ src/app/auth/types/authState.interface.ts
@@ -1,9 +1,10 @@
-import {CurrentUserInterface} from "../../shared/types/currentUser.interface";
-import {BackendErrorsInterface} from "../../shared/types/backendErrors.interface";
+import { CurrentUserInterface } from '../../shared/types/currentUser.interface';
+import { BackendErrorsInterface } from '../../shared/types/backendErrors.interface';
 
 export interface AuthStateInterface {
-  isSubmitting: boolean
-  currentUser: CurrentUserInterface | null
-  isLoggedIn: boolean | null
-  validationErrors: BackendErrorsInterface | null
+  isSubmitting: boolean;
+  isLoading: boolean;
+  currentUser: CurrentUserInterface | null;
+  isLoggedIn: boolean | null;
+  validationErrors: BackendErrorsInterface | null;
 }
